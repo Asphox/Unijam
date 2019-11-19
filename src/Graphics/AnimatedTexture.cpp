188fb2a1ea64837c1a0aa9044d27da135909e54e
Asphox <aspirine026@gmail.com>
2019-11-19 18:16:36 +0100

    debut

---
+++ src/Graphics/AnimatedTexture.cpp
@@ -0,0 +1,5 @@
+//
+// Created by asphox on 19/11/2019.
+//
+
+#include "AnimatedTexture.h"
---
+++ src/Graphics/AnimatedTexture.h
@@ -0,0 +1,15 @@
+//
+// Created by asphox on 19/11/2019.
+//
+
+#ifndef UNIJAM_ANIMATEDTEXTURE_H
+#define UNIJAM_ANIMATEDTEXTURE_H
+
+
+class AnimatedTexture
+{
+
+};
+
+
+#endif //UNIJAM_ANIMATEDTEXTURE_H
