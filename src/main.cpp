728832e3b1822370d429af230f73c46fc9328792
Asphox <aspirine026@gmail.com>
2019-11-22 23:13:28 +0100

    box2D

--- src/main.cpp
+++ src/main.cpp
@@ -3,57 +3,26 @@
 #include "System/Timer.h"
 #include "System/Thread.h"
 
+#include "Tiers/Box2D/Box2D.h"
+
 
 int main()
 {
+    b2Vec2 gravity(0.0f, -10.0f);
+    b2World world(gravity);
     std::cout.sync_with_stdio(false);
     sf::RenderWindow window(sf::VideoMode(800, 600), "SFML works!");
     window.setFramerateLimit(600);
-
-    AnimatedSprite sprite;
-    AnimatedSprite sprite2;
-    AnimatedSprite sprite3;
-    sprite.setUpdatePeriodMultiplier(10);
-
-    sf::Texture texture;
-    texture.loadFromFile("a.png");
-    sprite.setNbrSubTextures(5);
-    sprite.setTexture(texture);
-
-    sprite2 = sprite;
-    sprite2.move(100,0);
-    sprite3 = sprite;
-    sprite3.move(200,0);
-
-
-    Timer<AnimatedSprite> timer(10);
-    timer.addCallBack(&sprite,&AnimatedSprite::onTickAnimation);
-    timer.addCallBack(&sprite2,&AnimatedSprite::onTickAnimation);
-    timer.addCallBack(&sprite3,&AnimatedSprite::onTickAnimation);
-    timer.start();
 
 
     while (window.isOpen())
     {
         sf::Event event;
-        while (window.pollEvent(event))
-        {
+        while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed)
                 window.close();
-            if(event.type == sf::Event::KeyReleased)
-            {
-                static bool caca = true;
-                if(caca)
-                    timer.stop();
-                else
-                    timer.start();
-                caca = !caca;
-            }
         }
         window.clear();
-        window.draw(sprite);
-        window.draw(sprite2);
-        window.draw(sprite3);
         window.display();
     }
 
